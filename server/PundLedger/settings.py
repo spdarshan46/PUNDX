7ba8d167f98d272a5e84b34c74657f139708d3d5
spdarshan46 <spdarshan252@gmail.com>
2026-03-19 14:24:14 +0530

    admin

--- server/PundLedger/settings.py
+++ server/PundLedger/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # ─────────────────────────────────────────────────────────────
 #  STATIC FILES
 # ─────────────────────────────────────────────────────────────
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
